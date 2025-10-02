abbdda10fa42ca9a1816b3e7bd5251d5bbdca527
MrBacon470 <MrBacon470@outlook.com>
2025-10-02 15:44:00 -0500

    Some planning for events (not remotely done yet)

--- Scripts/events.js
+++ Scripts/events.js
@@ -5,5 +5,21 @@ const seasonalEventPool = []
 const weeklyEventPool = []
 
 function updateEventsHTML() {
-    
+
 }
+
+/**
+ * Weekly Event Structure
+ * Select from a random egg the player has unlocked (Only Unlocked Ones)
+ * Select from the types of challenges (Money Goal, Chicken Count, Eggs Produced)
+ */
+
+/**
+ * Seasonal Event Structure: Happens every 3 months, 4 Seasons a year
+ * Lasts for 2 months, Starts in December (winter season), March (spring season), June (summer season), September (fall season)
+ * Each season has a special gamemode: 
+ *  Manufacturing (Making Egg Factories to create a good), 
+ *  Battle (Auto battler where better eggs win more), 
+ *  Farming (Grow Eggs to Grow Eggs??)
+ *  4th One??
+ */
