3e20abccb7b2e109868a22b93a1a05f68339a515
MrBacon470 <MrBacon470@outlook.com>
2025-10-02 15:17:04 -0500

    Adding Events Script, More HTML Prototyping, Vault Upgrade Design Phase

---
+++ Scripts/events.js
@@ -0,0 +1,9 @@
+const vaultUpgradeCosts = []
+const ascensionPreservationRates = []
+let celestialEggBonus = D(1)
+const seasonalEventPool = []
+const weeklyEventPool = []
+
+function updateEventsHTML() {
+    
+}
--- Scripts/update.js
+++ Scripts/update.js
@@ -35,78 +35,90 @@ function updateHTML() {
     DOMCacheGetOrSet('ascSubButton3').style.display = data.legendaryResearch[3].gte(legendaryResearches[3].max) ? 'block' : 'none'
     //hm
     DOMCacheGetOrSet('tabButton5').style.display = data.unlockedEgg[3] === true ? 'block' : 'none'
-    if(data.currentTab === 0) {
-        updateEggPage()
-    }
-    else if(data.currentTab === 1) {
-        updateCommonResearchHTML()
-        for(let i = 0; i < 2; i++) {
-            DOMCacheGetOrSet(`auto${i}`).style.display = data.epicResearch[6+i].gte(epicResearches[6+i].maxLevel) ? 'inline-block' : 'none'
-            DOMCacheGetOrSet(`auto${i}`).classList = data.autoActive[i] ? 'greenButton' : 'redButton'
-            DOMCacheGetOrSet(`auto${i}`).innerText = data.autoActive[i] ? `${autoNames[i]}: On` : `${autoNames[i]}: Off`
-        }
-        DOMCacheGetOrSet(`auto3`).style.display = data.epicResearch[10].gte(epicResearches[10].maxLevel) ? 'inline-block' : 'none'
-        DOMCacheGetOrSet(`auto3`).classList = data.autoActive[3] ? 'greenButton' : 'redButton'
-        DOMCacheGetOrSet(`auto3`).innerText = data.autoActive[3] ? `${autoNames[3]}: On` : `${autoNames[3]}: Off`
-    }
-    else if(data.currentTab === 2) {
-        updateContractsHTML()
-    }
-    else if(data.currentTab === 3) {
-        if(data.currentSubTab[0] === 0) {
-            DOMCacheGetOrSet(`setTog0`).innerText = data.settingsToggles[0] ? `Notation: Mixed Sci` : `Notation: Sci`
-            DOMCacheGetOrSet(`setTog1`).innerText = data.settingsToggles[1] ? `Newsticker: On` : `Newsticker: Off`
-            DOMCacheGetOrSet(`setTog2`).innerText = data.settingsToggles[2] ? `Contract Notifications: On` : `Contract Notifications: Off`
-            DOMCacheGetOrSet(`setTog3`).innerText = data.settingsToggles[3] ? `Auto Promote Stops at: Enlightenment` : `Auto Promote Stops at: Universe`
-            DOMCacheGetOrSet(`setTog4`).innerText = data.settingsToggles[4] ? `Prestige Confirmation: On` : `Prestige Confirmation: Off`
-            DOMCacheGetOrSet(`setTog5`).innerText = data.settingsToggles[5] ? `Ascension Confirmation: On` : `Ascension Confirmation: Off`
-            DOMCacheGetOrSet(`setTog6`).innerText = data.settingsToggles[6] ? `Harvester Notifications: On` : `Harvester Notifications: Off`
-        }
-        else if(data.currentSubTab[0] === 1) {
-            updateStats()
-        }
-        else if(data.currentSubTab[0] === 3) {
-            DOMCacheGetOrSet('help0').style.display = data.hasPrestiged ? 'block' : 'none'
-            DOMCacheGetOrSet('help1').style.display = data.hasPrestiged ? 'block' : 'none'
-            DOMCacheGetOrSet('help2').style.display = data.unlockedEgg[3] ? 'block' : 'none'
-            DOMCacheGetOrSet('help3').style.display = data.unlockedContracts ? 'block' : 'none'
-            DOMCacheGetOrSet('help4').style.display = data.hasAscended ? 'block' : 'none'
-            DOMCacheGetOrSet('help5').style.display = data.hasAscended ? 'block' : 'none'
-            DOMCacheGetOrSet('help6').style.display = data.legendaryResearch[1].gte(legendaryResearches[1].max) ? 'block' : 'none'
-            DOMCacheGetOrSet('help7').style.display = data.legendaryResearch[1].gte(legendaryResearches[1].max) ? 'block' : 'none'
-            DOMCacheGetOrSet('help8').style.display = data.legendaryResearch[3].gte(legendaryResearches[3].max) ? 'block' : 'none'
-        }
-    }
-    else if(data.currentTab === 4) {
-        DOMCacheGetOrSet('soulEggText').innerText = `Soul Eggs: ${format(data.soulEggs)}\nBest Soul Eggs: ${format(data.bestSoulEggs)}\nEarnings Boost: x${format(soulEggBoost)}`
-        DOMCacheGetOrSet('prophecyEggText').innerText = `Prophecy Eggs: ${format(data.prophecyEggs)}\nSoul Boost: x${format(prophecyEggBoost)}\nContract Reward Boost: x${format(contractRewardBoost)}`
-        for(let i = 0; i < epicResearchCost.length; i++)
-            if(data.epicResearch[i].lt(epicResearches[i].maxLevel)) {
-                if(data.buyAmounts[1] === 0)
-                    DOMCacheGetOrSet(`er${i}`).classList = data.soulEggs.gte(epicResearchCostDisplay[i]) ? 'purpleButton' : 'redButton'
-                else if(data.buyAmounts[1] !== 0 && data.soulEggs.gte(epicResearchCost[i]))
-                    DOMCacheGetOrSet(`er${i}`).classList = data.soulEggs.gte(epicResearchCostDisplay[i]) ? 'purpleButton' : 'yellowButton'
-                else
-                    DOMCacheGetOrSet(`er${i}`).classList = 'redButton'
+    switch (data.currentTab) {
+        case 0:
+            updateEggPage();
+            break;
+        case 1:
+            updateCommonResearchHTML();
+            for (let i = 0; i < 2; i++) {
+                const autoElement = DOMCacheGetOrSet(`auto${i}`);
+                autoElement.style.display = data.epicResearch[6 + i].gte(epicResearches[6 + i].maxLevel) ? 'inline-block' : 'none';
+                autoElement.classList = data.autoActive[i] ? 'greenButton' : 'redButton';
+                autoElement.innerText = data.autoActive[i] ? `${autoNames[i]}: On` : `${autoNames[i]}: Off`;
             }
-            else
-                DOMCacheGetOrSet(`er${i}`).classList = 'blueButton'
-        for(let i = 0; i < epicResearches.length; i++) {
-            DOMCacheGetOrSet(`er${i}`).innerText = data.epicResearch[i].lt(epicResearches[i].maxLevel) ? `${epicResearches[i].name}\n${epicResearches[i].desc}\nLevel: ${toPlaces(data.epicResearch[i],0,epicResearches[i].maxLevel.add(Decimal.dOne))}/${toPlaces(epicResearches[i].maxLevel,0,epicResearches[i].maxLevel.add(Decimal.dOne))}\nCost: ${format(epicResearchCostDisplay[i])} Soul Eggs` : 
-            `${epicResearches[i].name}\n${epicResearches[i].desc}\nLevel: ${toPlaces(data.epicResearch[i],0,epicResearches[i].maxLevel.add(Decimal.dOne))}/${toPlaces(epicResearches[i].maxLevel,0,epicResearches[i].maxLevel.add(Decimal.dOne))}\nCost: [MAXED]`
-        }
-        DOMCacheGetOrSet('prophecyEggSoftCapText').innerText = prophecyEggBoost.gte(1e6) ? `Prophecy Egg Boost is being divided by: ${format(softCapAmts[1])}` : `Prophecy Egg Boost Softcap takes effect at ${format(D(1e6))}`
-        DOMCacheGetOrSet('auto2').style.display = data.epicResearch[9].gte(epicResearches[9].maxLevel) ? 'inline-block' : 'none'
-        DOMCacheGetOrSet(`auto2`).classList = data.autoActive[2] ? 'greenButton' : 'redButton'
-        DOMCacheGetOrSet(`auto2`).innerText = data.autoActive[2] ? `${autoNames[2]}: On` : `${autoNames[2]}: Off`
-        DOMCacheGetOrSet('auto4').style.display = data.legendaryResearch[2].gte(legendaryResearches[2].max) ? 'inline-block' : 'none'
-        DOMCacheGetOrSet(`auto4`).classList = data.autoActive[4] ? 'greenButton' : 'redButton'
-        DOMCacheGetOrSet(`auto4`).innerText = data.autoActive[4] ? `${autoNames[4]}: On` : `${autoNames[4]}: Off`
-    }
-    else if(data.currentTab === 5) {
-        updateEggspeditionsUI()
-    }
-    else if(data.currentTab === 7) {
-       updateAscensionHTML()
-    }
+            const auto3Element = DOMCacheGetOrSet(`auto3`);
+            auto3Element.style.display = data.epicResearch[10].gte(epicResearches[10].maxLevel) ? 'inline-block' : 'none';
+            auto3Element.classList = data.autoActive[3] ? 'greenButton' : 'redButton';
+            auto3Element.innerText = data.autoActive[3] ? `${autoNames[3]}: On` : `${autoNames[3]}: Off`;
+            break;
+        case 2:
+            updateContractsHTML();
+            break;
+        case 3:
+            switch (data.currentSubTab[0]) {
+                case 0:
+                    DOMCacheGetOrSet(`setTog0`).innerText = data.settingsToggles[0] ? `Notation: Mixed Sci` : `Notation: Sci`;
+                    DOMCacheGetOrSet(`setTog1`).innerText = data.settingsToggles[1] ? `Newsticker: On` : `Newsticker: Off`;
+                    DOMCacheGetOrSet(`setTog2`).innerText = data.settingsToggles[2] ? `Contract Notifications: On` : `Contract Notifications: Off`;
+                    DOMCacheGetOrSet(`setTog3`).innerText = data.settingsToggles[3] ? `Auto Promote Stops at: Enlightenment` : `Auto Promote Stops at: Universe`;
+                    DOMCacheGetOrSet(`setTog4`).innerText = data.settingsToggles[4] ? `Prestige Confirmation: On` : `Prestige Confirmation: Off`;
+                    DOMCacheGetOrSet(`setTog5`).innerText = data.settingsToggles[5] ? `Ascension Confirmation: On` : `Ascension Confirmation: Off`;
+                    DOMCacheGetOrSet(`setTog6`).innerText = data.settingsToggles[6] ? `Harvester Notifications: On` : `Harvester Notifications: Off`;
+                    break;
+                case 1:
+                    updateStats();
+                    break;
+                case 3:
+                    DOMCacheGetOrSet('help0').style.display = data.hasPrestiged ? 'block' : 'none';
+                    DOMCacheGetOrSet('help1').style.display = data.hasPrestiged ? 'block' : 'none';
+                    DOMCacheGetOrSet('help2').style.display = data.unlockedEgg[3] ? 'block' : 'none';
+                    DOMCacheGetOrSet('help3').style.display = data.unlockedContracts ? 'block' : 'none';
+                    DOMCacheGetOrSet('help4').style.display = data.hasAscended ? 'block' : 'none';
+                    DOMCacheGetOrSet('help5').style.display = data.hasAscended ? 'block' : 'none';
+                    DOMCacheGetOrSet('help6').style.display = data.legendaryResearch[1].gte(legendaryResearches[1].max) ? 'block' : 'none';
+                    DOMCacheGetOrSet('help7').style.display = data.legendaryResearch[1].gte(legendaryResearches[1].max) ? 'block' : 'none';
+                    DOMCacheGetOrSet('help8').style.display = data.legendaryResearch[3].gte(legendaryResearches[3].max) ? 'block' : 'none';
+                    break;
+            }
+            break;
+        case 4:
+            DOMCacheGetOrSet('soulEggText').innerText = `Soul Eggs: ${format(data.soulEggs)}\nBest Soul Eggs: ${format(data.bestSoulEggs)}\nEarnings Boost: x${format(soulEggBoost)}`;
+            DOMCacheGetOrSet('prophecyEggText').innerText = `Prophecy Eggs: ${format(data.prophecyEggs)}\nSoul Boost: x${format(prophecyEggBoost)}\nContract Reward Boost: x${format(contractRewardBoost)}`;
+            for (let i = 0; i < epicResearchCost.length; i++) {
+                const erElement = DOMCacheGetOrSet(`er${i}`);
+                if (data.epicResearch[i].lt(epicResearches[i].maxLevel)) {
+                    if (data.buyAmounts[1] === 0)
+                        erElement.classList = data.soulEggs.gte(epicResearchCostDisplay[i]) ? 'purpleButton' : 'redButton';
+                    else if (data.buyAmounts[1] !== 0 && data.soulEggs.gte(epicResearchCost[i]))
+                        erElement.classList = data.soulEggs.gte(epicResearchCostDisplay[i]) ? 'purpleButton' : 'yellowButton';
+                    else
+                        erElement.classList = 'redButton';
+                } else {
+                    erElement.classList = 'blueButton';
+                }
+            }
+            for (let i = 0; i < epicResearches.length; i++) {
+                DOMCacheGetOrSet(`er${i}`).innerText = data.epicResearch[i].lt(epicResearches[i].maxLevel)
+                    ? `${epicResearches[i].name}\n${epicResearches[i].desc}\nLevel: ${toPlaces(data.epicResearch[i], 0, epicResearches[i].maxLevel.add(Decimal.dOne))}/${toPlaces(epicResearches[i].maxLevel, 0, epicResearches[i].maxLevel.add(Decimal.dOne))}\nCost: ${format(epicResearchCostDisplay[i])} Soul Eggs`
+                    : `${epicResearches[i].name}\n${epicResearches[i].desc}\nLevel: ${toPlaces(data.epicResearch[i], 0, epicResearches[i].maxLevel.add(Decimal.dOne))}/${toPlaces(epicResearches[i].maxLevel, 0, epicResearches[i].maxLevel.add(Decimal.dOne))}\nCost: [MAXED]`;
+            }
+            DOMCacheGetOrSet('prophecyEggSoftCapText').innerText = prophecyEggBoost.gte(1e6) ? `Prophecy Egg Boost is being divided by: ${format(softCapAmts[1])}` : `Prophecy Egg Boost Softcap takes effect at ${format(D(1e6))}`;
+            DOMCacheGetOrSet('auto2').style.display = data.epicResearch[9].gte(epicResearches[9].maxLevel) ? 'inline-block' : 'none';
+            DOMCacheGetOrSet(`auto2`).classList = data.autoActive[2] ? 'greenButton' : 'redButton';
+            DOMCacheGetOrSet(`auto2`).innerText = data.autoActive[2] ? `${autoNames[2]}: On` : `${autoNames[2]}: Off`;
+            DOMCacheGetOrSet('auto4').style.display = data.legendaryResearch[2].gte(legendaryResearches[2].max) ? 'inline-block' : 'none';
+            DOMCacheGetOrSet(`auto4`).classList = data.autoActive[4] ? 'greenButton' : 'redButton';
+            DOMCacheGetOrSet(`auto4`).innerText = data.autoActive[4] ? `${autoNames[4]}: On` : `${autoNames[4]}: Off`;
+            break;
+        case 5:
+            updateEggspeditionsUI();
+            break;
+        case 7:
+            updateAscensionHTML();
+            break;
+        case 8:
+            updateEventsHTML()
+}
+
 }
