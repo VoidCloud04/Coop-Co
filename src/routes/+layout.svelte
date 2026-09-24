bb2e76b4dcfe2d7aad2807cb596f97ebc727986d
VoidCloud04 <MrBacon470@outlook.com>
2026-09-24 14:27:32 -0500

    Redoing styling + adding  the svelte components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+	import '$lib/styles/global.scss';
 	let { children } = $props();
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import ButtonGroup from "$lib/components/ButtonGroup.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import Newsticker from "$lib/components/Newsticker.svelte";
+    import Ascension from "./Ascension.svelte";
+
+    import Contracts from "./Contracts.svelte";
+
+    import Egg from "./Egg.svelte";
+    import Eggspeditions from "./Eggspeditions.svelte";
+    import Prestige from "./Prestige.svelte";
+    import Research from "./Research.svelte";
+    import Settings from "./Settings.svelte";
+
+    // Imports
+
+    // Variable Definitions
+    let tabIndex = $state(-1)
+</script>
+
+<Header />
+<Newsticker />
+<ButtonGroup />
+
+{#if tabIndex === 0}
+    <Egg />
+{:else if tabIndex === 1}
+    <Research />
+{:else if tabIndex === 2}
+    <Prestige />
+{:else if tabIndex === 3}
+    <Contracts />
+{:else if tabIndex === 4}
+    <Eggspeditions />
+{:else if tabIndex === 5}
+    <Ascension />
+{:else if tabIndex === 6}
+    <Settings />
+{:else}
+    <h1>Invalid Tab Selected</h1>
+    <button class="button-blue">Hello I'm a Button</button>
+{/if}
+
